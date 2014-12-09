5dcecf2ffa10151950f2e38cde976bffaf31de2f
sbagroy986 <shrey14099@iiitd.ac.in>
2014-12-09 15:40:25 +0530

    Changes to search

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,11 +1,14 @@
 class HomeController < ApplicationController
 	def index
+		t=0
 		if params[:search]
-			if Customer.search(params[:search]).include? current_user.customers
+			if current_user.customers.exists?(:mobile => params[:search])
 				@customers = Customer.search(params[:search]).order("created_at DESC")
+				t=1
 			end
-			else
-				@customers = current_user.customers
+		end
+		if t == 0
+			@customers = current_user.customers
 		end
 	end
 end
